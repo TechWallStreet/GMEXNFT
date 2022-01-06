21b1d94f5d6d63862b7db283c1c420915e88e364
TechWallStreet <91001309+TechWallStreet@users.noreply.github.com>
2022-01-05 21:31:13 -0800

    Create GMEXNFTcallingsuper.sol

---
+++ GMEXNFTcallingsuper.sol
@@ -0,0 +1,16 @@
+// contracts/ModifiedAccessControl.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract ModifiedAccessControl is AccessControl {
+    function revokeRole(bytes32 role, address account) public override {
+        require(
+            role != DEFAULT_ADMIN_ROLE,
+            "ModifiedAccessControl: cannot revoke default admin role"
+        );
+
+        super.revokeRole(role, account);
+    }
+}
